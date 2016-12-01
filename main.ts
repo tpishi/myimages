2b87efdb391cbae29513af183b95961e769d6571
Toru Ishikawa <v.t.ishi.v@gmail.com>
2016-12-01 22:56:33 +0900

    Refactoring
    setTimeout 500millis to 0

--- main.ts
+++ main.ts
@@ -34,8 +34,7 @@ function stat(path:string):Promise<Array<string>> {
         return;
       }
       if (stats.isDirectory()) {
-        const paths = await dir(path);
-        resolve(paths);
+        resolve(await dir(path));
       } else if (path.endsWith('.jpg') ||
                  path.endsWith('.JPG') ||
                  path.endsWith('.jpeg') ||
@@ -50,60 +49,41 @@ function stat(path:string):Promise<Array<string>> {
 
 async function calcHash(path) {
   return new Promise((resolve, reject) => {
-    console.log('path:' + path);
-    const hash = crypto.createHash('sha1');
-    const data = fs.readFileSync(path);
-    hash.update(data);
-    const result = hash.digest().toString('hex');
-    console.log('result:' + result);
-    resolve(result);
-  });
-}
-
-function wait(idle) {
-  return new Promise((resolve, reject) => {
     setTimeout(() => {
-      resolve();
-    }, idle);
+      const hash = crypto.createHash('sha1');
+      const data = fs.readFileSync(path);
+      hash.update(data);
+      resolve(hash.digest().toString('hex'));
+    }, 0);
   });
 }
 
-let processing:number = 0;
-let total:number = 0;
 stat(process.argv[2]).then((files) => {
-  total = files.length;
+  let processing:number = 0;
+  let total:number = files.length;
   const observer = Rx.Observable.create(async (obs) => {
-    // TODO: まだここがダメ。
-    let index = 0;
-    for (index = 0; index < total; index++) {
-      let hash = await calcHash(files[index]);
-      console.log('hash:' + hash);
-      await wait(100);
-      console.log('wait finish');
+    // Here, forEach cannot be used.
+    for (let file of files) {
       obs.next({
-        path: files[index],
-        hash: hash
+        path: file,
+        hash: await calcHash(file)
       });
-      if (index === total - 1) {
-        obs.complete();
-      }
     }
+    obs.complete();
     return () => {
       console.log('disposed');
     };
   });
   observer.subscribe((x) => {
     processing++;
-    console.log('x:' + x + ':' + processing);
   }, (err) => {
     console.log('error');
   }, () => {
     console.log('complete');
   });
+  http.createServer((request, response) => {
+    response.write('hello:' + processing + '/' + total);
+    response.end();
+  }).listen(8080);
 });
 
-http.createServer((request, response) => {
-  console.log('request');
-  response.write('hello:' + processing + '/' + total);
-  response.end();
-}).listen(8080);
