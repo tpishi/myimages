5368625612381297d04d7cf88948c43e78bbef37
Toru Ishikawa <v.t.ishi.v@gmail.com>
2016-12-02 22:59:51 +0900

    Add progress bar

--- websrc/client.ts
+++ websrc/client.ts
@@ -3,17 +3,20 @@ function getSummary() {
   const req = new XMLHttpRequest();
   req.open('GET', 'http://127.0.0.1:8080/cache/summary', true);
   req.onload = (e) => {
-    const elem = document.getElementById('test');
+    const elem = document.getElementById('myprogress');
     if (req.readyState == 4) {
       if (req.status === 200) {
         const json = JSON.parse(req.responseText);
         if (json.totalImages === 0) {
-          elem.innerText = 'loading';
           setTimeout(getSummary, 1000);
         } else if (json.preparedImages < json.totalImages) {
+          const valuer = Math.floor(json.preparedImages*100/json.totalImages);
+          $('.progress-bar').css('width', valuer+'%').attr('aria-valuenow', valuer);
           elem.innerText = json.preparedImages + '/' + json.totalImages;
           setTimeout(getSummary, 1000);
         } else if (json.preparedImages === json.totalImages) {
+          elem['aria-valuenow'] = '"' + json.preparedImages + '"';
+          elem['aria-valuemax'] = '"' + json.totalImages + '"';
           elem.innerText = json.preparedImages + '/' + json.totalImages;
         }
       } else {
